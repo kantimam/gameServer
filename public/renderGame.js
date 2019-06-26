99252d250baff836b8d0b44aa5bb19ec383a95a8
Nyannazz <kantemir.imam@gmail.com>
2019-06-26 03:00:51 +0200

    code clean up

--- public/renderGame.js
+++ public/renderGame.js
@@ -1,179 +1,270 @@
-
 import Character from './Character.js';
 import Projectile from './Projectile.js';
 
-export default function renderGame(socket){
-    
+export default function renderGame(socket) {
 
-    
-    
-    const app = new PIXI.Application();
-    app.renderer.resize(window.innerWidth,window.innerHeight )
-    console.log(app.renderer.width)
-    const b=new Bump(PIXI);
-    const renderer=document.getElementById("rendererDiv").appendChild(app.view);
-    renderer.style.touchAction="auto";
-    
-    
-    PIXI.Loader.shared
-      .add("sprites/penguin.json")
-      .add("ground","textures/ground.jpg")
-      .load(setup);
-    
-    
-    
-    function setup() {
-        app.stage.sortableChildren=true;
 
-        let sheet=PIXI.Loader.shared.resources["sprites/penguin.json"].spritesheet
-        let background= new PIXI.TilingSprite(PIXI.Loader.shared.resources.ground.texture,app.renderer.width,app.renderer.height)
-        
-        app.stage.addChild(background)
 
-        function createCharacter(parent, posX=100, posY=100, size=2){
-          const sprite = new Character(sheet,true);
-          sprite.zIndex=10;
-          sprite.scale.x=size;
-          sprite.scale.y=size;
-          sprite.position.x=posX;
-          sprite.position.y=posY;
-          sprite.animationSpeed=0.167;
-          sprite.play();
-          parent.addChild(sprite);
-          sprite.interactive=true;
-          
-          return sprite 
+
+  const app = new PIXI.Application();
+  app.renderer.resize(window.innerWidth, window.innerHeight)
+  console.log(app.renderer.width)
+  const b = new Bump(PIXI);
+  const renderer = document.getElementById("rendererDiv").appendChild(app.view);
+  renderer.style.touchAction = "auto";
+
+
+  PIXI.Loader.shared
+    .add("sprites/penguin.json")
+    .add("ground", "textures/ground.jpg")
+    .load(setup);
+
+
+
+  function setup() {
+    app.stage.sortableChildren = true;
+
+    let sheet = PIXI.Loader.shared.resources["sprites/penguin.json"].spritesheet
+    let background = new PIXI.TilingSprite(PIXI.Loader.shared.resources.ground.texture, app.renderer.width, app.renderer.height)
+
+    app.stage.addChild(background)
+
+    function createCharacter(parent, posX = 100, posY = 100, size = 2) {
+      const sprite = new Character(sheet, true);
+      sprite.zIndex = 10;
+      sprite.scale.x = size;
+      sprite.scale.y = size;
+      sprite.position.x = posX;
+      sprite.position.y = posY;
+      sprite.animationSpeed = 0.167;
+      sprite.play();
+      parent.addChild(sprite);
+      sprite.interactive = true;
+
+      return sprite
+    }
+
+
+
+    let up,
+      down,
+      left,
+      right,
+      shoot = false;
+    document.addEventListener('keydown', event => {
+
+      switch (event.keyCode) {
+        case 83: {
+
+          down = true;
+          break
         }
-        
-        const sprite=createCharacter(app.stage)
-    
-    
-        let up, 
-            down, 
-            left, 
-            right, 
-            shoot=false;
-        document.addEventListener('keydown',event=>{
-        
-          switch(event.keyCode){
-            case 83:{
-    
-              down=true; 
-              break
-            }
-            case 68:{
-    
-              right=true; 
-              break
-            }
-            case 87:{
-    
-              up=true; 
-              break;
-            }
-            case 65:{
-     
-              left=true; 
-              break;
-            }
- 
-          }
-        })
-         document.addEventListener('keyup',event=>{
-           sprite.moveVector={x:0,y:0}
-           switch(event.keyCode){
-            case 83:{
-              down=false; 
-              break
-            }
-            case 68:{
-              right=false;
-              break
-            }
-            case 87:{
-              up=false; 
-              break;
-            }
-            case 65:{
-              left=false;  
-              break;
-            }
-   
-          }
-        })
-        renderer.addEventListener('click',(event)=>{
-          if(sprite.shootReady){
-            console.log(Object.keys(gameState.players))
-            sprite.shoot(()=>shootFromChar(sprite.getBounds(),event.clientX,event.clientY));
-          }
-        })
-    
-    
-        let projectiles=[]
-        function shootFromChar(bounds, mouseX, mouseY){
-          const position={x: bounds.x+bounds.width/2,y: bounds.y+bounds.height/2};
-          const newProjectile=new Projectile(sheet.animations.idle)
-          newProjectile.position=position;
-          newProjectile.flyToMouse(position.x,position.y,mouseX,mouseY)
-          newProjectile.animationSpeed=0.167;
-          newProjectile.zIndex=1;
-          newProjectile.gotoAndPlay(0);
-          projectiles.push(newProjectile)
-          app.stage.addChild(newProjectile)
-    
-        }
-        app.stage.sortableChildren=true;
-    
-        
-        let collision=0;
-        let removedProjectile=0;
-        app.renderer.render(app.stage);
-    
-    
-        let gameState={
-          x:0,
-          y:0
-        }
-        
-        // get data from server
-        let playerState=0
-        socket.on('state',(state)=>{
-          gameState=state;
-          for(let player in state.players){
-            playerState=state.players[player];
-          }
-        })
+        case 68: {
 
-        socket.on('newactor',(actor)=>{
-          createCharacter(app.stage, actor.x, actor.y, actor.size)
-        })
-
-        function sendUpdatesToServer(player){
-          if(player){
-            socket.emit('playerMovement',{
-              ...player.moveVector,animation: player.mpAnimation
-             })
-          }
-          
+          right = true;
+          break
         }
-        app.ticker.add(() => {
+        case 87: {
 
-          sprite.movementFromInputMultiplayer(up,right,down,left)
-    
-          sendUpdatesToServer(sprite)
-  
-          if(playerState){
-            sprite.position.x=playerState.x;
-            sprite.position.y=playerState.y;
-            sprite.selectAnimation(playerState.animation || "idle")
-          }
-          
-       });
-    
-       
-       
-    
+          up = true;
+          break;
+        }
+        case 65: {
+
+          left = true;
+          break;
+        }
+
       }
+    })
+    document.addEventListener('keyup', event => {
+ 
+      switch (event.keyCode) {
+        case 83: {
+          down = false;
+          break
+        }
+        case 68: {
+          right = false;
+          break
+        }
+        case 87: {
+          up = false;
+          break;
+        }
+        case 65: {
+          left = false;
+          break;
+        }
+
+      }
+    })
+    renderer.addEventListener('click', (event) => {
+      console.log(gameState)
+      console.log(players)
+      if (players[socket.id] && players[socket.id].shootReady) {
+        players[socket.id].shoot(() => spawnProjectileAtServer(players[socket.id].getBounds(), event.clientX, event.clientY));
+      }
+    })
+
+
+    const players = {}
+    /* players[socket.id]=createCharacter(app.stage) */
+
+
+
+    let projectiles = []
+
+    function shootFromChar(bounds, mouseX, mouseY) {
+      const position = {
+        x: bounds.x + bounds.width / 2,
+        y: bounds.y + bounds.height / 2
+      };
+      const newProjectile = new Projectile(sheet.animations.idle)
+      newProjectile.position = position;
+      newProjectile.flyToMouse(position.x, position.y, mouseX, mouseY)
+      newProjectile.animationSpeed = 0.167;
+      newProjectile.zIndex = 1;
+      newProjectile.gotoAndPlay(0);
+      projectiles.push(newProjectile)
+      app.stage.addChild(newProjectile)
+
+    }
+
+    function createProjectile(bounds, mouseX, mouseY) {
+      const position = {
+        x: bounds.x + bounds.width / 2,
+        y: bounds.y + bounds.height / 2
+      };
+      const newProjectile = new Projectile(sheet.animations.idle)
+      newProjectile.position = position;
+      newProjectile.animationSpeed = 0.167;
+      newProjectile.zIndex = 1;
+      newProjectile.gotoAndPlay(0);
+      projectiles.push(newProjectile)
+      app.stage.addChild(newProjectile)
+    }
+
+    let projectileId = 0;
+
+    function spawnProjectileAtServer(bounds, mouseX, mouseY) {
+      socket.emit('shoot', {
+        x: mouseX,
+        y: mouseY,
+        id: projectileId
+      })
+      createProjectile(bounds, mouseX, mouseY)
+      projectileId++;
+    }
+
+
+    let collision = 0;
+    let removedProjectile = 0;
+
+
+    let gameState = {
+
+    }
+
+    // get data from server
+    let projectileState = 0;
+    socket.on('state', (state) => {
+      gameState = state;
+      /* for(let player in state.players){
+        playerArray.push(state.players[player]);
+        
+      } */
+      projectileState = state.projectiles
+    })
+    socket.emit('newPlayer')
+
+    socket.on('newactor', (actor) => {
+      console.log(actor)
+      players[actor.id]=createCharacter(app.stage, actor.x, actor.y, actor.size)
+    })
+
+    /* function sendUpdatesToServer(player){
+      if(player){
+        socket.emit('playerMovement',{
+          ...player.moveVector,animation: player.mpAnimation
+         })
+      }
+      
+    } */
+
+    function sendUpdatesToServer(moveVector, mpAnimation) {
+        socket.emit('playerMovement', {
+          ...moveVector,
+          animation: mpAnimation
+        })
+      
+    }
+
+    function movementFromInput(up,right,down,left) {
+      const speed=10;
+      const moveVector={x:0,y:0}
+      let mpAnimation='idle'
+      if (down && right) {
+        moveVector.x = speed * Math.cos(45 * Math.PI / 180);
+        moveVector.y = moveVector.x;
+      } else if (down && left) {
+        moveVector.y = speed * Math.cos(45 * Math.PI / 180);
+        moveVector.x = -moveVector.y;
+      } else if (up && right) {
+        moveVector.y = -speed * Math.cos(45 * Math.PI / 180);
+        moveVector.x = -moveVector.y;
+      } else if (up && left) {
+        moveVector.x = -speed * Math.cos(45 * Math.PI / 180);
+        moveVector.y = moveVector.x;
+      } else if (up) {
+        mpAnimation = "walkBack"
+        moveVector.y = -speed;
+        moveVector.x = 0;
+      } else if (right) {
+        mpAnimation = "walkRight"
+        moveVector.y = 0
+        moveVector.x = speed;
+      } else if (down) {
+        mpAnimation = "walkFront"
+        moveVector.y = speed;
+        moveVector.x = 0;
+      } else if (left) {
+        mpAnimation = "walkLeft"
+        moveVector.y = 0
+        moveVector.x = -speed;
+      }
+      sendUpdatesToServer(moveVector, mpAnimation)
+    }
+    app.renderer.render(app.stage);
+
+    app.ticker.add(() => {
+      movementFromInput(up,right,down,left)
+
+      if (projectileState && projectileState.length === projectiles.length) {
+        for (let i = 0; i < projectiles.length; i++) {
+          projectiles[i].position.x = projectileState[i].posX;
+          projectiles[i].position.y = projectileState[i].posY;
+        }
+      }
+
+
+
+      if (gameState.players/*  && Object.entries(players).legnth>0 */) {
+        for (let player in gameState.players) {
+          try{
+            players[player].position.x = gameState.players[player].x;
+            players[player].position.y = gameState.players[player].y;
+            players[player].selectAnimation(gameState.players[player].animation || "idle")
+          }
+          catch{
+
+          }
+          
+        }
+      }
+
+    });
+
+  }
   return renderer
 }
-
--- server.js
+++ server.js
@@ -12,24 +12,34 @@ app.get('/', function (req, res) {
 
 
 const gameState = {
-   players: {}
+   players: {},
+   projectiles: []
 }
-
+const projectileSpeed=6;
+let socketId=0;
 io.on('connection',(socket)=>{
     console.log(`user connected with the id: ${socket.id}`)
+    socketId=socket.id;
     socket.on('disconnect',()=>{
         console.log(`disconnected userId: ${socket.id}`)
         delete gameState.players[socket.id]
     })
     socket.on('newPlayer',()=>{
         console.log("newPlayer")
-        gameState.players[socket.id]={
-            x: 100,
-            y: 200,
-            animation: 'idle',
-            health: 100
+        const newPlayer={
+            id: socket.id,
+            x:Math.floor(Math.random()*600),
+            y:Math.floor(Math.random()*400),
+            animation: "idle",
+            health: 100,
+            size: 3
         }
+        gameState.players[socket.id]=newPlayer
+        console.log(gameState)
+        io.sockets.emit('newactor', newPlayer);
+
     })
+
     socket.on('playerMovement',(playerMovement)=>{
         const currentPlayer=gameState.players[socket.id];
         if(currentPlayer){
@@ -38,16 +48,46 @@ io.on('connection',(socket)=>{
             currentPlayer.animation=playerMovement.animation;
         }
     })
+    socket.on('shoot',(projectile)=>{
+        const currentPlayer=gameState.players[socket.id]
+        console.log(socket.id)
+        if(currentPlayer){
+            const angle=Math.atan2(projectile.x-currentPlayer.x, projectile.y-currentPlayer.y);
+            gameState.projectiles.push({
+                id: projectile.id,
+                posX: currentPlayer.x,
+                posY: currentPlayer.y,
+                moveX: projectileSpeed*Math.sin(angle),
+                moveY: projectileSpeed*Math.cos(angle)
+            })
+        }
+        
+    })
 })
 
 setInterval(() => {
+    gameState.projectiles.forEach(projectile=>{
+        projectile.posX+=projectile.moveX;
+        projectile.posY+=projectile.moveY;
+    })
     io.sockets.emit('state', gameState);
   }, 1000/60);
 
 
 
 
-setInterval(() => {
+/* setInterval(() => {
+    io.sockets.emit('newactor', {
+        id: socketId,
+        x:Math.floor(Math.random()*600),
+        y:Math.floor(Math.random()*400),
+        animation: "idle",
+        health: 100,
+        size: 3
+    });
+  }, 4000); */
+
+  function spawnActor(){
     io.sockets.emit('newactor', {
         x:Math.floor(Math.random()*600),
         y:Math.floor(Math.random()*400),
@@ -55,7 +95,7 @@ setInterval(() => {
         health: 100,
         size: 3
     });
-  }, 4000);
+  }
 
 
 
